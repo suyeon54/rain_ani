20580f800e7cbb0d5de53d06fa039a3b15c19598
suyeon54 <waterkite54@gmail.com>
2024-07-12 11:05:25 +0900

    버전1

---
+++ script/action.js
@@ -0,0 +1,18 @@
+$('.icon_music').click(function(){
+    $(this).toggleClass('on')
+    /* $('audio').fadeToggle() */
+
+    var audio = $('audio#music')[0];
+
+    if(audio.paused) {
+        audio.play();
+    } else {
+        audio.puase();
+    }
+
+    $('wrap').toggleClass('on');
+    $('.text_box').toggleClass('on')
+
+    /* $('.wrap').toggleClass('on') */
+})
+
